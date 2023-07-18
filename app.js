7c898a9ba466421d1e660f4d02cac6a35c0c38d5
ImGabeDelgado <139909341+ImGabeDelgado@users.noreply.github.com>
2023-07-18 12:52:22 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const modal = document.querySelector(".modal");
+const openModal = document.querySelector(".open");
+const closeModal = document.querySelector(".close");
+
+openModal.addEventListener("click", () => {
+    modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+    modal.close();
+});
+
+document.querySelector(".submit-button").addEventListener("click", () => {
+    modal.close();
+});
